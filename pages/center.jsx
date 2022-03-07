c43ebf905e9b9425f9910da4ef02bc7195886896
wuxiao-web <2011707158@qq.com>
2022-03-07 22:58:19 +0800

    111

--- pages/center.jsx
+++ pages/center.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React , {useState} from "react";
 import { useRouter } from "next/router";
-import { Arrow, Edit } from '@react-vant/icons';
+import {  Edit } from '@react-vant/icons';
 import { Card, Image, Button, Toast, Space, Cell, Divider } from 'react-vant';
 import Layout from "../components/Layout";
-export default function Center() {
-  const users = {
-      userId: "user_01",
-      userName: "coderfan",
-      password: "coderfan@123",
-      slogan: "hello, our todolist project and the ant design mobile conponent",
-      picUrl: "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+export default function Center({req}) {
+ const {userId} = req.cookies;
+ const [users, setUsers] = useState({})
+ const newUsers = {}
+ const getUser = () =>{
+  fetch("http://localhost:3000/api/users/" + userId,{method:'GET'})
+  .then((response) => response.json()) 
+  .then((responseData) => {
+    // console.log(responseData.entity);
+    newUsers = responseData.entity;
+   
+    setUsers(newUsers)
+    
+  })
  }
+ getUser();
+
  const router = useRouter();
  return (
     <Layout>
@@ -20,13 +29,13 @@ export default function Center() {
           <Cell
             style={{ marginTop: 15 }}
             size = "large"
-            title="coderfan"
-            label="ID: 20220316"
+            title= {users.userName}
+            label={"ID：" + users.userId}
             icon={
               <Image
                 width={65}
                 height={65}
-                src="https://cdn.jsdelivr.net/gh/3lang3/react-vant@main/public/home-music-card-1.jpg"
+                src={users.picUrl}
               />
             }
           />
@@ -42,7 +51,7 @@ export default function Center() {
             alignItems: 'center',
           }}
         >
-          我是小妖怪，逍遥又自在
+          {users.slogan}
         </Card.Body>
         <Card.Footer border>
           <Space>
