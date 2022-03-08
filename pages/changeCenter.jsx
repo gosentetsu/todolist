0e3bc72c5334c7e08cb6b5126e64e37156eb0416
wuxiao-web <2011707158@qq.com>
2022-03-08 14:47:31 +0800

    修改资料和密码

--- pages/changeCenter.jsx
+++ pages/changeCenter.jsx
@@ -10,17 +10,28 @@ import NavBarCard from "../components/NavBarCard";
 
 export default function ChangeCenter() {
   const router = useRouter();
-  const [form] = Form.useForm();
-  const onFinish = (values) => {
-    console.log(values);
-  };
-  const [username, setUsername] = useState('');
+  function updateCenter(values){
+    const options = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    };
+
+    fetch("http://localhost:3000/api/users/user_01", options)
+      .then((response) => response.json())
+      .then((res) => {
+        let { message } = res;
+        console.log(message)
+        if (message === "success") {
+          router.push("/center");
+        }
+      });
+  }
   return (
     <div>
       <NavBarCard title="修改个人信息" path="/center" />
       <Form
-        form={form}
-        onFinish={onFinish}
+        onFinish={updateCenter}
         footer={
           <div style={{ margin: '16px 16px 0' }}>
             <Button round nativeType="submit" type="primary" block size = "mini">
@@ -31,9 +42,9 @@ export default function ChangeCenter() {
       >
       
         <Form.Item
-          name="uploader"
+          
           label="头像"
-          rules={[{ required: true, message: '请选择文件' }]}
+          
           initialValue={[
             {
               url: 'https://img.yzcdn.cn/vant/sand.jpg',
@@ -44,21 +55,18 @@ export default function ChangeCenter() {
         >
           <Uploader />
         </Form.Item>
-        <Form.Item name="textarea" label="用户名">
+        <Form.Item name="userName" label="用户名" rules={[{ required: true, message: '用户名不能为空' }]}>
           <Field
-              value={username}
-              error
               required
-              label="用户名"
               placeholder="请输入用户名"
-              onChange={setUsername}
             />
         </Form.Item>
-        <Form.Item name="textarea" label="个性签名">
+        <Form.Item name="slogan" label="个性签名">
           <Field 
               rows={3} 
               autosize 
-              type="textarea" 
+              type="textarea"
+              placeholder="请输入个性签名"
               maxlength={140} 
               showWordLimit />
         </Form.Item>
--- pages/changePwd.jsx
+++ pages/changePwd.jsx
@@ -1,35 +1,58 @@
 import React, { useState } from 'react';
 import { useRouter } from "next/router";
 import {
-  Field, Button, Cell
+  Field, Button, Toast, Form
 } from 'react-vant';
 import NavBarCard from "../components/NavBarCard";
 
 export default function ChangePwd() {
-  const [phone, setPhone] = useState('');
   const router = useRouter();
+  function updatePwd(values){
+    const options = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+    };
+
+    fetch("http://localhost:3000/api/users/user_01", options)
+      .then((response) => response.json())
+      .then((res) => {
+        let { message } = res;
+        console.log(message)
+        if (message === "success") {
+          router.push("/login");
+        }
+      });
+  }
   return (
     <div>
       <NavBarCard title="修改密码" path="/center" />
-      <Cell.Group>
-        <Field
-          required
-          label="新密码"
-          placeholder="请输入新密码"
-        />
-        <Field
-          required
-          label="新密码"
-          placeholder="请再次输入新密码"
-        />
-        
-      </Cell.Group>
-
-      <div style={{ margin: '16px 16px 0' }}>
-          <Button round nativeType="submit" type="primary" block size = "mini">
+      <Form
+      showValidateMessage={false}
+      onFinish={updatePwd}
+      footer={
+        <div style={{ margin: '16px 16px 0' }}>
+          <Button round nativeType="submit" type="primary" block>
             提交
           </Button>
-      </div>
+        </div>
+      }
+    >
+      <Form.Item
+        tooltip={{
+          message:
+            '请输入原密码',
+        }}
+        rules={[{ required: true, message: '请填写原密码' }]}
+        name="password"
+        label="原密码"
+      >
+        <Field placeholder="请输入新密码" type = "password"/>
+      </Form.Item>
+      <Form.Item rules={[{ required: true, message: '请填写新密码' }]} name="newPass" label="新密码">
+        <Field placeholder="请输入新密码" type = "password"/>
+      </Form.Item>
+    </Form>
 
     </div>
   );
