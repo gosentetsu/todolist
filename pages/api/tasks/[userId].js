1ebf17d40de367d4db79438d2718e91396ecd8ef
gosentetsu <gosentetsu@outlook.com>
2022-03-08 13:37:08 +0800

    fixed status data type

--- pages/api/tasks/[userId].js
+++ pages/api/tasks/[userId].js
@@ -1,7 +1,7 @@
 import dbConnect from "../../../lib/dbConnect";
 import Task from "../../../models/Task";
 import Relation from "../../../models/Relation";
-import verifyToken from "../../../lib/verifyToken"
+import verifyToken from "../../../lib/verifyToken";
 /**
  * @swagger
  * /api/tasks/[userId]:
@@ -18,48 +18,48 @@ export default async function handler(req, res) {
     query: { userId },
     method,
     body,
-    cookies
+    cookies,
   } = req;
   let result, task, relation, taskId;
   let idLength = 6;
   let idChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-  if(!cookies || !cookies.token || userId !== verifyToken(cookies.token)){
-    return res.status(400).json({ message: "please sign in"});
+  if (!cookies || !cookies.token || userId !== verifyToken(cookies.token)) {
+    return res.status(400).json({ message: "please sign in" });
   }
   await dbConnect();
 
   switch (method) {
     case "GET":
       //获取useId对应的所有taskId
-      relation = await Relation.find({userId: userId});
+      relation = await Relation.find({ userId: userId });
       //遍历查询Task表得到相应的待办事项
       let tasks = [];
-      for(let rel of relation){
-        task = await Task.findOne({taskId: rel.taskId}).lean();
+      for (let rel of relation) {
+        task = await Task.findOne({ taskId: rel.taskId }).lean();
         delete task._id;
         tasks.push(task);
       }
-      res.status(200).json({ 
+      res.status(200).json({
         message: "success",
-        list: tasks
+        list: tasks,
       });
       break;
     case "POST":
-      while(true){
+      while (true) {
         taskId = "T_";
-        for(let i = 0; i < idLength; i++){
-          let ind = Math.floor(Math.random()*62);
+        for (let i = 0; i < idLength; i++) {
+          let ind = Math.floor(Math.random() * 62);
           taskId += idChar[ind];
         }
         // taskId查重
-        result = await Task.findOne({taskId: taskId});
-        if(!result) break;
+        result = await Task.findOne({ taskId: taskId });
+        if (!result) break;
       }
-      
+
       task = {
         taskId: taskId,
-        status: "not complete",
+        status: false,
         content: body.content,
         comment: body.comment,
         tag: body.tag,
@@ -70,43 +70,43 @@ export default async function handler(req, res) {
 
       relation = {
         taskId: taskId,
-        userId: userId
+        userId: userId,
       };
       await Relation.create(relation);
-      
-      res.status(200).json({ 
+
+      res.status(200).json({
         message: "success",
-        entity: {taskId: taskId}
+        entity: { taskId: taskId },
       });
       break;
     case "PUT":
-      if(!body.taskId){
-        return res.status(400).json({ message: "taskId is required"});
+      if (!body.taskId) {
+        return res.status(400).json({ message: "taskId is required" });
       }
-      result = await Task.updateOne({taskId: body.taskId}, {$set: body});
-      if(result.modifiedCount !== 1){
-        return res.status(400).json({ message: "update failed"});
+      result = await Task.updateOne({ taskId: body.taskId }, { $set: body });
+      if (result.modifiedCount !== 1) {
+        return res.status(400).json({ message: "update failed" });
       }
-      res.status(200).json({ message: "success"});
+      res.status(200).json({ message: "success" });
       break;
     case "DELETE":
-      result = await Task.deleteOne({taskId: body.taskId});
-      if(result.deletedCount !== 1){
-        return res.status(400).json({ message: "delete failed"});
+      result = await Task.deleteOne({ taskId: body.taskId });
+      if (result.deletedCount !== 1) {
+        return res.status(400).json({ message: "delete failed" });
       }
 
       result = await Relation.deleteOne({
         taskId: body.taskId,
-        userId: userId
+        userId: userId,
       });
-      if(result.deletedCount !== 1){
-        return res.status(400).json({ message: "delete failed"});
+      if (result.deletedCount !== 1) {
+        return res.status(400).json({ message: "delete failed" });
       }
 
-      res.status(200).json({ message: "success"});
+      res.status(200).json({ message: "success" });
       break;
     default:
-      res.status(400).json({ message: "null"});
+      res.status(400).json({ message: "null" });
       break;
   }
 }
