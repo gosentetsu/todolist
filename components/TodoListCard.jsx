8657e2f711d1acb5b2798776af29e41fd69a5b84
gosentetsu <gosentetsu@outlook.com>
2022-03-24 22:00:34 +0800

    update todolist card design, add modal when clicked item

--- components/TodoListCard.jsx
+++ components/TodoListCard.jsx
@@ -1,40 +1,65 @@
-import React, {useState} from "react";
-import { List, Checkbox, Tag, Space, Empty } from "antd-mobile";
-import { Dialog, Field, Toast } from 'react-vant';
+import React, { useState } from "react";
+import { List, Checkbox, Tag, Space, Empty, Modal, Toast } from "antd-mobile";
+import { Dialog, Field } from "react-vant";
 import {
   TagOutline,
   CalendarOutline,
   StarOutline,
   AddCircleOutline,
-  AddOutline
+  AddOutline,
+  TeamOutline,
 } from "antd-mobile-icons";
+import jsCookie from "js-cookie";
 export default function TodoListCard(props) {
   let { content, header, onItemChange } = props;
   const [visible, setVisible] = useState(false);
   const [tempTaskId, setTempTaskId] = useState();
-  const [tempName, setTempName] = useState()
+  const [tempName, setTempName] = useState();
   const addCooperatorList = (name) => {
     const options = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({taskId:tempTaskId, coworkerUserName:name}),
+      body: JSON.stringify({ taskId: tempTaskId, coworkerUserName: name }),
     };
     fetch("/api/tasks/coworker", options)
       .then((response) => response.json())
       .then((res) => {
         let { message } = res;
-        Toast({
-          message: message,
-          forbidClick: true,
+        Toast.show({
+          content: message,
         });
         if (message === "success") {
         }
       });
-  }
-  const addAction = (item) => {
+  };
+  const handleAddCoworker = (item) => {
     setVisible(true);
     setTempTaskId(item.taskId);
-  }
+  };
+  const handleUpdateItem = (item) => {
+    console.log(item);
+    /**
+     * invoke add item function here
+     */
+  };
+  const handleDeleteItem = (item) => {
+    console.log(item.taskId);
+    const options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: item.taskId,
+    };
+
+    fetch("/api/tasks/" + jsCookie.get("userId"))
+      .then((response) => response.json())
+      .then((response) => {
+        console.log("deleted");
+        Toast.show({
+          content: response.message,
+        });
+      })
+      .catch((err) => console.error(err));
+  };
   return (
     <div>
       <List mode="card" header={header}>
@@ -65,11 +90,64 @@ export default function TodoListCard(props) {
             description={item.comment}
             prefix={
               <Checkbox
+                id="checkbox"
                 defaultChecked={item.status}
                 onChange={(val) => onItemChange(item, val)}
               />
             }
-            extra={<AddCircleOutline onClick={() => addAction(item)} />}
+            arrow
+            onClick={(e) => {
+              if (e.target.className !== "adm-list-item-content-main") return;
+              Modal.show({
+                closeOnAction: true,
+                closeOnMaskClick: true,
+                header: (
+                  <Space wrap>
+                    <Tag color="success" fill="solid">
+                      <TagOutline />
+                      {item.tag}
+                    </Tag>
+                    <Tag color="warning" fill="solid">
+                      <StarOutline />
+                      {item.importance}
+                    </Tag>
+                    <Tag color="primary" fill="outline">
+                      <CalendarOutline />
+                      {new Date(item.beginTime).toLocaleDateString() +
+                        " - " +
+                        new Date(item.endTime).toLocaleDateString()}
+                    </Tag>
+                    <Tag color="primary" fill="outline">
+                      <TeamOutline />
+                      合作者:
+                      {item.coworkers.map((i) => (
+                        <Tag color="primary" fill="solid" key={i.userId}>
+                          {i.userName}
+                        </Tag>
+                      ))}
+                    </Tag>
+                  </Space>
+                ),
+                title: item.content,
+                actions: [
+                  {
+                    key: "delete",
+                    text: "删除该待办",
+                    onClick: () => handleDeleteItem(item),
+                  },
+                  {
+                    key: "update",
+                    text: "更新待办",
+                    onClick: () => handleUpdateItem(item),
+                  },
+                  {
+                    key: "addCoworer",
+                    text: "添加合作者",
+                    onClick: () => handleAddCoworker(item),
+                  },
+                ],
+              });
+            }}
           >
             {item.content}
           </List.Item>
@@ -83,15 +161,19 @@ export default function TodoListCard(props) {
         }}
       >
         <Field
-      center
-      clearable
-      label="合作者"
-      placeholder="请输入合作者username"
-      rightIcon = {<AddOutline />}
-      onClickRightIcon={()=>{addCooperatorList(tempName)}}
-      onChange={(value)=>{setTempName(value)}}
-    />
-  </Dialog>
+          center
+          clearable
+          label="合作者"
+          placeholder="请输入合作者username"
+          rightIcon={<AddOutline />}
+          onClickRightIcon={() => {
+            addCooperatorList(tempName);
+          }}
+          onChange={(value) => {
+            setTempName(value);
+          }}
+        />
+      </Dialog>
     </div>
   );
 }
